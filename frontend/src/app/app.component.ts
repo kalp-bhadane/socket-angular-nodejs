688cce13a4bfed65356bb6002898f8a57da68b9b
kalp-bhadane <kalpeshbhadane77@gmail.com>
2024-09-11 00:32:21 +0530

    personal chat init

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { ChatService } from './service/chat.service';
 export class AppComponent implements OnInit {
   @ViewChild('messageBox') messageBox?: ElementRef;
   message?: string;
+  selectedUser: any;
   messages: any[] = [
     { userId: 1, message: 'Kaise ho?' },
     { userId: 2, message: 'Hum to majeme hai' },
@@ -47,35 +48,21 @@ export class AppComponent implements OnInit {
       name: 'Jyoti Bhadane',
       image: './../assets/bhau-vahini.jpg'
     },
-    {
-      id: 1,
-      name: 'Kalpesh Bhadane',
-      image: './../assets/kalpesh.png'
-    },
-    {
-      id: 2,
-      name: 'Sau Bhadane',
-      image: './../assets/sau-kalpesh.jpg'
-    },
-    {
-      id: 3,
-      name: 'Umesh Bhadane',
-      image: './../assets/umesh.jpg'
-    }
   ]
   constructor(private chatService: ChatService) { }
 
   ngOnInit(): void {
     this.chatService.getMessage().subscribe((data: any) => {
-      this.message = data.message;
-      this.messages.push(data);
+      if(data.userId !== this.loginUser.id) {
+        this.messages.push(data);
+      }
       this.scrollToBottom();
     });
   }
 
   sendMessage(newMessage: any) {
     this.message = newMessage.value;
-    const messageData = { message: this.message, userId: this.loginUser.id };
+    const messageData = { message: this.message, userId: this.loginUser.id, roomId: this.getRoomId() };
     this.chatService.sendMessage(messageData);
     this.messages.push(messageData);
     this.message = newMessage.value = '';
@@ -89,12 +76,23 @@ export class AppComponent implements OnInit {
   private scrollToBottom(): void {
     try {
       if (this.messageBox) {
-        this.messageBox.nativeElement.scrollTop = this.messageBox.nativeElement.scrollHeight+100;
-        
+        this.messageBox.nativeElement.scrollTop = this.messageBox.nativeElement.scrollHeight + 100;
+
       }
     } catch (err) {
       console.error('Scrolling failed', err);
     }
+  }
+
+  onUserSelect(user: any) {
+    this.selectedUser = user;
+    this.messages = [];
+    this.chatService.joinRoom({ roomId: this.getRoomId() });
+  }
+
+  getRoomId() {
+    return this.loginUser.id < this.selectedUser.id ?
+      `user${this.loginUser.id}_user${this.selectedUser.id}` : `user${this.selectedUser.id}_user${this.loginUser.id}`
   }
 }
 
